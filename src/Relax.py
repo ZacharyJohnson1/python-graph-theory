f10c3a042d0400cf6d619841b1ea7cf8458efb13
ZacharyJohnson1 <65374624+ZacharyJohnson1@users.noreply.github.com>
2022-04-19 09:16:29 -0500

    add set_distance and set_parent methods

--- src/Relax.py
+++ src/Relax.py
@@ -3,6 +3,6 @@ class Relax:
     @staticmethod
     def relax(u, v, w):
 
-        if v.distance > u.distance + w:
-            v.distance = u.distance + w
-            v.parent = u
+        if v.get_distance() > u.get_distance() + w:
+            v.set_distance(u.get_distance() + w)
+            v.set_parent(u)
