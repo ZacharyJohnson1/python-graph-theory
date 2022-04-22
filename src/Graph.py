891e068e8821b64b8d423edb7354a8d94f4477b6
ZacharyJohnson1 <65374624+ZacharyJohnson1@users.noreply.github.com>
2022-04-22 15:28:35 -0500

    add vertices and edges as optional args

--- src/Graph.py
+++ src/Graph.py
@@ -2,26 +2,37 @@ from Edge import Edge
 
 class Graph:
 
-    def __init__(self):
+    def __init__(self, vertices=[], edges=[]):
 
-        self.vertices = []
-        self.edges = []
+        self.vertices = vertices
+        self.edges = edges
 
 
     def insert_vertex(self, vertex):
         
         for v in self.vertices:
-            if v.id == vertex.id:
+            if v.get_id() == vertex.get_id():
+                v = vertex
                 return
         self.vertices.append(vertex)
+
+    
+    def get_vertex(self, id):
+
+      for v in self.vertices:
+        if v.get_id() == id:
+          return v
+      return None
             
 
     def define_edge(self, vertex_one, vertex_two, weight=0):
         
         edge = Edge(vertex_one, vertex_two, weight)
         if edge not in self.edges:
-            vertex_one.adjacency_list.append(vertex_two)
-            vertex_two.adjacency_list.append(vertex_one)
+            edge.u.set_adjacency_list(edge.v)
+            edge.v.set_adjacency_list(edge.u)
+            self.insert_vertex(edge.u)
+            self.insert_vertex(edge.v)
             self.edges.append(edge)
 
         else:
@@ -31,8 +42,10 @@ class Graph:
     def add_edge(self, edge):
 
         if edge not in self.edges:
-            edge.u.adjacency_list.append(edge.v)
-            edge.v.adjacency_list.append(edge.u)
+            edge.u.set_adjacency_list(edge.v)
+            edge.v.set_adjacency_list(edge.u)
+            self.insert_vertex(edge.u)
+            self.insert_vertex(edge.v)
             self.edges.append(edge)
         else:
             self.edges[self.edges.index(edge)].w = edge.weight
