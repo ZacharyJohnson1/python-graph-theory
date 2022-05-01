a2ae8bbf9068cae5546eeae1ea766c88fefe866a
ZacharyJohnson1 <65374624+ZacharyJohnson1@users.noreply.github.com>
2022-05-01 16:07:47 -0500

    fix scope issue with time

--- src/DFS.py
+++ src/DFS.py
@@ -1,55 +1,58 @@
+from Vertex import Vertex
 from Graph import Graph
-import math
 
-class Vertex:
-
-    def __init__(self, id, color='WHITE', distance=math.inf, time=0, parent=None):
-
-        self.id = id
-        self.color = color
-        self.distance = distance
-        self.time = time
-        self.parent = parent
-        self.adjacency_list = []
-
-
-    def __ge__(self, v):
-        
-        return True if self.distance > v.distance else False
-
-
-    def __lt__(self, v):
-
-        return True if self.distance < v.distance else False
-
-
-    def __eq__(self, v):
-        
-        return True if self.id == v.id else False
-
+time = 0
 
 def dfs(G):
     
     for v in G.vertices:
-        v.color = 'WHITE'
-        v.parent = None
-    time = 0
+        v.set_color('WHITE')
+        v.set_parent(None)
+    #time = 0
     
     for v in G.vertices:
-        if v.color == 'WHITE':
+        if v.get_color() == 'WHITE':
             dfs_visit(G, v)
 
 
 def dfs_visit(G, u):
-    
-    time += 1
-    u.distance = time
-    u.color = 'GRAY'
 
-    for v in G.vertices[u].adjacency_list:
-        if v.color == 'WHITE':
-            v.parent = u
-            dfs_visit(G, v)
-    u.color = 'BLACK'
+    global time
     time += 1
-    u.finish = time
+    u.set_distance(time)
+    u.set_color('GRAY')
+
+    for v in G.vertices[G.vertices.index(u)].adjacency_list:
+        if v.get_color() == 'WHITE':
+            v.set_parent(u)
+            dfs_visit(G, v)
+    u.set_color('BLACK')
+    time += 1
+    u.set_finish_time(time)
+    print(f'{u.get_id()} - distance: {u.get_distance()}, finish: {u.get_finish_time()}')
+
+if __name__ == '__main__':
+    
+    u = Vertex('U')
+    v = Vertex('V')
+    w = Vertex('W')
+    x = Vertex('X')
+    y = Vertex('Y')
+    z = Vertex('Z')
+
+    G = Graph()
+
+    G.insert_vertex(u)
+    G.insert_vertex(v)
+    G.insert_vertex(w)
+    G.insert_vertex(x)
+    G.insert_vertex(y)
+    G.insert_vertex(z)
+
+    G.define_edge(u, v)
+    G.define_edge(w, x)
+    G.define_edge(x, y)
+    G.define_edge(y, z)
+    G.define_edge(v, w)
+
+    dfs(G)
